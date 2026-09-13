ad1d092bb0c3c4f9aa7ed1af239a50ce9c0f9a9b
Ripley Bobko <rbobko08@gmail.com>
2026-09-12 21:12:25 -0700

    Fix merge issues
    oh yeah we so back

--- Assets/Scripts/Pot_Handler.cs
+++ Assets/Scripts/Pot_Handler.cs
@@ -6,13 +6,8 @@ public class Pot_Handler : MonoBehaviour
 {
    public void itemDropped(GameObject item)
     {
-<<<<<<< Updated upstream
-        print(item.name);
-        if (item.name.StartsWith("Salt"))
-=======
         //print(item.name);
         if (item.name.Contains("Salt") || item.name.Contains("Bacteria"))
->>>>>>> Stashed changes
         {
             Destroy(item);
         }
